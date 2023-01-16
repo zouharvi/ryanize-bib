c5f15f95ac1c69e7d822e4ea2e3bf722121eccef
Vilém Zouhar <vilem.zouhar@gmail.com>
2023-01-16 19:38:26 +0100

    first draft

--- src/src/worker_website.ts
+++ src/src/worker_website.ts
@@ -1,88 +1,157 @@
 import { log_data } from "./connector";
 import { DEVMODE } from "./globals";
 import { getIndicies } from "./utils";
+import { parseBibFile, type } from "bibtex";
 
 let main_text_area = $("#main_text_area")
 let active_response_area_template = $("#active_response_area_template")
 
-const zip = (a, b) => a.map((k, i) => [k, b[i]]);
+let CHECK_ARXIV = false
+let CHECK_PUBLISHER = false
+let CHECK_CAPITAL = false
+let CHECK_URL = false
 
-function load_headers() {
-    console.log(globalThis.data.length)
-    $("#progress").html(`
-        <strong>Progress:</strong> ${globalThis.data_i + 1}/${globalThis.data.length},
-        <strong>UID:</strong> ${globalThis.uid}
-    `)
-}
-
-function setup_main_text() {
-    // let html_text = `<b>${globalThis.data_now["title"]}</b><br>${globalThis.data_now["text"]}`;
-    let out_html = ""
-    zip(globalThis.data_now["texts"], globalThis.data_now["imgs"]).forEach((element) => {
-        let text_path = element[0];
-        let text_el = `<iframe src="texts/${text_path}" frameborder="0" scrolling="no" onload="resizeIframe(this)"></iframe>`;
-        out_html += text_el;
-        let imgs = element[1];
-        if(imgs.length == 0) {
-            return;
+function formatText(text: string) {
+    return text.split("\n").map((line) => {
+        let prevLine = ""
+        // hacky way to remove space around equality
+        while (prevLine != line) {
+            prevLine = line
+            line = line.replace("= ", "=").replace(" =", "=")
         }
-        // TODO: take just the first image for now
-        let img_path = imgs[0]
-        let response_template = active_response_area_template.html();
-        response_template = response_template.replaceAll("IMAGE_TEMPLATE", img_path);
-        // remove lazy loading flag
-        response_template = response_template.replaceAll('loading="lazy"', "");
-        out_html += response_template;
-    })
-    main_text_area.html(out_html);
+        console.log(line)
+        // bracket everything
+        line=line.replace('="', '={')
+        line = (line+",").replace(",,", ",")
+        line=line.replace('",', '},')
+        if (!line.includes("=")) {
+            return line
+        }
+        if (line.includes("{") && line.includes("}")) {
+            return line
+        }
+        line = line.replace("=", "={")
+        
+        // manual add trailing commas
+        // we can add them everywhere because in `dumpEntries` we unpack everything
+        // if (!line.includes(",")) {
+        //     line += ","
+        // }
+        let lastComma = line.lastIndexOf(",")
+        line = line.substring(0, lastComma) + '},' + line.substring(lastComma + 1)
+        return line
+    }).join("\n")
 }
 
-function load_cur_text() {
-    load_headers()
-    setup_main_text()
-}
-
-function load_thankyou() {
-    // TODO: wait for data sync
-    load_headers()
-    let html_text = `Thank you for participating in our study. `;
-    if (globalThis.uid.startsWith("prolific_pilot_1")) {
-        html_text += `<br>Please click <a href="https://app.prolific.co/submissions/complete?cc=C67G3X5Y">this link</a> to go back to Prolific. `
-        html_text += `Alternatively use this code <em>C67G3X5Y</em>.`
+function flattenBraced(object) {
+    if (typeof(object) != "object") {
+        return object
     }
-    main_text_area.html(html_text);
+    return "{" + object["data"].map(flattenBraced).join("") + "}"
+}
+
+function dumpEntries(entries: Object) {
+    let out = ""
+    for (let key in entries) {
+        let entry = entries[key]
+        out += `@${entry["type"]}{${key},\n`
+
+        let hasURL = false
+        for(let field in entry["fields"]) {
+            if (field == "url") {
+                hasURL = true
+            }
+
+            let fieldDataTxt = ""
+            let fieldData = entry["fields"][field]["data"]
+            while (fieldData.length == 1 && typeof(fieldData[0]) == "object") {
+                fieldData = fieldData[0]["data"]
+            }
+            for (let element in fieldData) {
+                fieldDataTxt += flattenBraced(fieldData[element])
+            }
+
+            if (CHECK_ARXIV && field=="journal") {
+                fieldDataTxt = fieldDataTxt.replaceAll("arXiv", '<span class="line_warning">arXiv</span>')
+                fieldDataTxt = fieldDataTxt.replaceAll("arxiv", '<span class="line_warning">arxiv</span>')
+            }
+
+            if (CHECK_CAPITAL && field =="title") {
+                // BibTeX removes double spaces anyway
+                fieldDataTxt = fieldDataTxt.replace(/\s+/g,' ')
+                let words = fieldDataTxt.split(" ")
+                // reset output
+                fieldDataTxt = ""
+                for(let word_i in words) {
+                    let word = words[word_i]
+                    if (word.length >= 2 && word.toUpperCase() == word && (!word.includes("{") || !word.includes("}"))) {
+                        fieldDataTxt += `<span class="line_warning">${word}</span> `
+                        continue
+                    }
+                    let word_i_num = parseInt(word_i)
+                    if (word_i_num > 0 && (!word.includes("{") || !word.includes("}"))){
+                        let prev_word = words[`${word_i_num-1}`]
+                        if (prev_word.at(-1) == ":") {
+                            fieldDataTxt += `<span class="line_warning">${word}</span> `
+                            continue
+                        }
+                    }
+                    fieldDataTxt += word + " "
+                }
+                fieldDataTxt = fieldDataTxt.trim()
+            }
+
+            if (CHECK_PUBLISHER && field == "publisher") {
+                if (["article", "journal", "inproceeding"].includes(entry["type"])) {
+                    out += `    ${field}=<span class="line_warning">{${fieldDataTxt}}</span>,\n`
+                }
+                continue
+            }
+
+            if (field == "url" && fieldDataTxt == "URL MISSING") {
+                if (CHECK_URL) {
+                    out += `    <span class="line_warning">url={URL MISSING}</span>,\n`
+                }
+            } else {
+                out += `    ${field}={${fieldDataTxt}},\n`
+            }
+
+        }
+        
+        if (!hasURL && CHECK_URL) {
+            out += `    <span class="line_warning">url={URL MISSING}</span>,\n`
+        }
+
+        out += `}\n\n`
+    }
+    return out
 }
 
 function setup_navigation() {
-    $("#but_next").on("click", () => {
-        globalThis.data_i += 1;
+    $("#button_go").on("click", () => {
+        CHECK_URL = $("#check_url").is(":checked")
+        CHECK_ARXIV = $("#check_arxiv").is(":checked")
+        CHECK_PUBLISHER = $("#check_publisher").is(":checked")
+        CHECK_CAPITAL = $("#check_capital").is(":checked")
 
-        // globalThis.data_log.times.push(Date.now())
-        // log_data(globalThis.data_log)
+        // $("#main_editable").html($("#main_editable").html()+" ")
+        let text = $("#main_editable").html() as string
+        // remove html tags
+        text = text.replaceAll("<br>", "\n")
+        // text = text.replace(/<(.|\n)*?>/g, '')
+        text = formatText(text)
+        const bibFile = parseBibFile(text)
 
-        if (globalThis.data_i >= globalThis.data.length) {
-            globalThis.data_i = 0;
-            load_thankyou()
-        } else {
-            globalThis.data_now = globalThis.data[globalThis.data_i];
-            load_cur_text()
-        }
-
-        $("#but_prev").prop("disabled", globalThis.data_i == 0);
-        $("#but_next").prop("disabled", globalThis.data_i == globalThis.data.length-1);
+        text = dumpEntries(bibFile["entries$"])
+        $("#main_editable").html(text)
     })
 
-    $("#but_prev").on("click", () => {
-        globalThis.data_i -= 1;
-        $("#but_prev").prop("disabled", globalThis.data_i == 0);
-        $("#but_next").prop("disabled", globalThis.data_i == globalThis.data.length-1);
-
-        globalThis.data_now = globalThis.data[globalThis.data_i];
-        load_cur_text()
-    })
-
-    $("#but_prev").prop("disabled", true);
+    // $("#main_editable").on("input paste", (event) => {
+    //     // $("#main_editable").html($("#main_editable").html() + " ")
+    //     console.log($("#main_editable").html().includes("<br>"))
+    // })
+    
 }
 
 
-export { setup_navigation, load_cur_text }
+export { setup_navigation }
