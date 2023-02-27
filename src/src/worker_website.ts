60b122cd892d6966f2c3bfabca32abd081186870
Vilém Zouhar <vilem.zouhar@gmail.com>
2023-02-27 11:15:04 +0100

    automatic fix button

--- src/src/worker_website.ts
+++ src/src/worker_website.ts
@@ -23,10 +23,10 @@ function preprocessTextMain(text: string) {
     text = text.replace(/\n\s*\n/g, '\n');
 
     text = text.replace(/<(.|\n)*?>/g, '')
-    
+
     // replace "}   ," with "}," (makes us or BibTeX flip out otherwise)
     text = text.replace(/\}\s+,/g, '},')
-    
+
     // for some reason running it twice makes it works
     // we're going full LaTeX here
     text = preprocessText(text)
@@ -38,11 +38,11 @@ function preprocessText(text: string) {
     // make sure that the input is somewhat canonized and readable by
     // the bibtex module which is otherwise conservative
     let insideEntry = false
-    let output:Array<string> = []
+    let output: Array<string> = []
 
     text.split("\n").forEach((line) => {
         line = line.trim()
-        
+
         let prevLine = ""
         // hacky way to remove space around equality
         while (prevLine != line) {
@@ -54,7 +54,7 @@ function preprocessText(text: string) {
 
         // this is a very hacky way (as this whole function) to do stuff absed on if we're inside a value or not
         if (insideEntry) {
-            output[output.length-1] += " " + line
+            output[output.length - 1] += " " + line
 
             if (!line.includes("={") && line.replaceAll(",", "").endsWith("}")) {
                 insideEntry = false
@@ -62,10 +62,10 @@ function preprocessText(text: string) {
             return
         }
         // bracket everything
-        line=line.replace('="', '={')
-        line = (line+",").replace(",,", ",")
-        line=line.replace('",', '},')
-        
+        line = line.replace('="', '={')
+        line = (line + ",").replace(",,", ",")
+        line = line.replace('",', '},')
+
         if (!line.includes("=")) {
             output.push(line)
             return
@@ -77,8 +77,8 @@ function preprocessText(text: string) {
         // special case where the entry starts here but doesn't end here
         if (line.includes("={") && !line.replaceAll(",", "").endsWith("}")) {
             insideEntry = true
-            line = (line+"$").replace(",$", "")
-            
+            line = (line + "$").replace(",$", "")
+
             output.push(line)
             return
         }
@@ -88,38 +88,50 @@ function preprocessText(text: string) {
 
         output.push(line)
     })
-    
+
     return output.join("\n")
 }
 
-
+let fix_actions = new Map<string, Array<(string) => string>>()
+function fix_entry(key: string) {
+    console.log(`fixing entry for ${key}`)
+    console.log(fix_actions)
+    let text = main_editable.text();
+    fix_actions[key].forEach((action) => { text = action(text) });
+    main_editable.html(text)
+    // trigger lcick
+    $("#button_go").trigger("click")
+}
+// make it global
+globalThis.fix_entry = fix_entry
 
 function processEntry(key, entry): string {
     // big function which should be split up
     // does both checking and formatting the output
+    fix_actions[key] = new Array<(string) => string>();
 
     let hasURL = false
     let hasDOI = false
     let title = ""
-    for(let field in entry["fields"]) {
+    for (let field in entry["fields"]) {
         if (field == "url") {
             hasURL = true
         }
         if (field == "doi") {
             hasDOI = true
         }
-        if (field == "title"){
+        if (field == "title") {
             title = flattenBraced(entry["fields"][field])
         }
     }
+    let out = `@${entry["type"]}{${key}, FIX_BUTTON_PLACEHOLDER\n`
     let search_url = `https://scholar.google.com/scholar?hl=en&as_sdt=0%2C5&q=${title.replaceAll(' ', '+').replaceAll('\'', '').replaceAll('{', '').replaceAll('}', '')}`
     let search_button = `<input class="search_button" type="button" value="search" onclick="window.open('${search_url}', '_blank')">`
-    let out = `@${entry["type"]}{${key}, ${search_button}\n`
 
-    for(let field in entry["fields"]) {
+    for (let field in entry["fields"]) {
         let fieldDataTxt = ""
         let fieldData = entry["fields"][field]["data"]
-        while (fieldData.length == 1 && typeof(fieldData[0]) == "object") {
+        while (fieldData.length == 1 && typeof (fieldData[0]) == "object") {
             fieldData = fieldData[0]["data"]
         }
         for (let element in fieldData) {
@@ -133,63 +145,92 @@ function processEntry(key, entry): string {
             }
             seen_titles.add(title_hash)
         }
-        
-        if (CHECK_ABSTRACT && field=="abstract") {
-            fieldDataTxt = '<span class="line_warning">' + fieldDataTxt + '</span>'
+
+        if (CHECK_ABSTRACT && field == "abstract") {
+            let fieldDataTxtClean = fieldDataTxt.replace("</span>", "").replace('<span class="line_warning">', "")
+            // remove the field as a fix
+            fix_actions[key].push((text) => {
+                return text.replace(`abstract={${fieldDataTxtClean}},`, "")
+            })
+            fieldDataTxt = '<span class="line_warning">' + fieldDataTxtClean + '</span>'
         }
 
-        if (CHECK_ARXIV && field=="journal") {
+        if (CHECK_ARXIV && field == "journal") {
             fieldDataTxt = fieldDataTxt.replaceAll("arXiv", '<span class="line_warning">arXiv</span>')
             fieldDataTxt = fieldDataTxt.replaceAll("arxiv", '<span class="line_warning">arxiv</span>')
         }
 
-        if (CHECK_CAPITAL && field =="title") {
+        if (CHECK_CAPITAL && field == "title") {
             // BibTeX removes double spaces anyway
-            fieldDataTxt = fieldDataTxt.replace(/\s+/g,' ')
+            fieldDataTxt = fieldDataTxt.replace(/\s+/g, ' ')
             let words = fieldDataTxt.split(" ")
             // reset output
             fieldDataTxt = ""
-            for(let word_i in words) {
+            let correct_words = []
+            let words_need_fixing = false
+            for (let word_i in words) {
                 let word = words[word_i]
                 if (shouldCapitalProtect(word) && (!word.includes("{") || !word.includes("}"))) {
                     fieldDataTxt += `<span class="line_warning">${word}</span> `
+                    correct_words.push(`{${word}}`)
+                    words_need_fixing = true
                     continue
                 }
                 let word_i_num = parseInt(word_i)
-                if (word_i_num > 0 && (!word.includes("{") || !word.includes("}"))){
-                    let prev_word = words[`${word_i_num-1}`]
+                if (word_i_num > 0 && (!word.includes("{") || !word.includes("}"))) {
+                    let prev_word = words[`${word_i_num - 1}`]
                     if (prev_word.at(-1) == ":") {
                         fieldDataTxt += `<span class="line_warning">${word}</span> `
+                        correct_words.push(`{${word}}`)
+                        words_need_fixing = true
                         continue
                     }
                 }
                 fieldDataTxt += word + " "
+                correct_words.push(`${word}`)
             }
             fieldDataTxt = fieldDataTxt.trim()
+            if (words_need_fixing) {
+                fix_actions[key].push((text) => {
+                    return text.replace(words.join(" "), correct_words.join(" "))
+                })
+            }
         }
 
         if (CHECK_PUBLISHER && field == "publisher") {
             if (["article", "journal", "inproceeding"].includes(entry["type"])) {
-                out += `    ${field}=<span class="line_warning">{${fieldDataTxt}}</span>,\n`
+                out += `    publisher=<span class="line_warning">{${fieldDataTxt}}</span>,\n`
+                // remove the field as a fix
+                fix_actions[key].push((text) => {
+                    return text.replace(`publisher={${fieldDataTxt}},`, "")
+                })
             }
             continue
         }
 
         if (field == "url" && fieldDataTxt == "URL/DOI MISSING") {
             if (CHECK_URL) {
-                out += `    url=<span class="line_warning">{URL/DOI MISSING}</span>,\n`
+                out += `    url=<span class="line_warning">{URL/DOI MISSING}</span>, ${search_button}\n`
             }
         } else {
             out += `    ${field}={${fieldDataTxt}},\n`
         }
 
     }
-    
+
     if (!hasURL && !hasDOI && CHECK_URL) {
-        out += `    url=<span class="line_warning">{URL/DOI MISSING}</span>,\n`
+        out += `    url=<span class="line_warning">{URL/DOI MISSING}</span>, ${search_button}\n`
     }
 
     out += `}`
+
+    if (fix_actions[key].length == 0) {
+        // remove placeholder if it was not used
+        out = out.replace("FIX_BUTTON_PLACEHOLDER", "")
+    } else {
+        out = out.replace("FIX_BUTTON_PLACEHOLDER", `<input class="search_button" type="button" value="fix" onclick="fix_entry('${key}')">`)
+    }
+
 
     return out
 }
@@ -214,7 +255,7 @@ function checkEntriesAndDump(entries: Object) {
 
 function check_no_duplicate_keys(text: string) {
     // take only the key
-    let keys = [...text.matchAll(/@\w+\{(\w+),/g)].map((a)=>a[1])
+    let keys = [...text.matchAll(/@\w+\{(\w+),/g)].map((a) => a[1])
     let keys_seen = new Set<string>()
     let keys_dups = new Array<string>()
     keys.forEach((key) => {
@@ -241,7 +282,7 @@ function setup_navigation() {
         let keys_dups = check_no_duplicate_keys(text)
         if (keys_dups.length > 0) {
             $("#process_log").html(`Won't parse the bibfile because is contains the following duplicate entries [<em>${keys_dups.join(", ")}</em>]`)
-            return 
+            return
         }
 
         // yank comments
@@ -252,30 +293,30 @@ function setup_navigation() {
         // clear seen cache
         seen_titles = new Set<string>()
         let bibFile;
-        try {   
+        try {
             bibFile = parseBibFile(text)
         } catch {
             $("#process_log").html(`Can't parse the bibfile :-(<br>Make sure it's formatted correctly`)
             return
         }
-        
+
         let output = checkEntriesAndDump(bibFile["entries$"])
         text = output[0] as string
 
         let logMessage = `${output[1]} problems found`
 
-        if (comments.length >0) {
+        if (comments.length > 0) {
             logMessage += "\nComments were moved to the top (they are not fully supported yet)"
             main_editable.html(comments + "\n\n" + text)
         } else {
             main_editable.html(text)
         }
-        
+
         $("#process_log").html(logMessage)
 
         main_editable.scrollTop(0)
         main_editable.scrollLeft(0)
-    })    
+    })
 }
 
 
